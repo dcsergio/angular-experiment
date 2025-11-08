a898b90b073820733daa1621f74a754d10c3e106
Sergio Della Cioppa <dcsergio07@gmail.com>
2025-11-08 15:43:55 +0100

    aa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {AComponent} from './a/a.component';
+import {BComponent} from './b/b.component';
+
+const routes: Routes = [
+  {path: 'a', component: AComponent},
+  {path: 'b', component: BComponent},
+  {path: '', redirectTo: 'a', pathMatch: 'full'}
+];
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  standalone: true,
+  imports: [RouterModule],
+  template: `
+    <header>
+      <nav>
+        <a routerLink="/a">A</a>
+        <br>
+        <a routerLink="/b">B</a>
+      </nav>
+    </header>
+    <main>
+      <router-outlet></router-outlet>
+    </main>
+    <footer>
+      © 2025 Il Mio Sito Angular
+    </footer>
+  `
 })
 export class AppComponent {
-  title = 'ang-app';
 }
